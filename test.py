3b476c05e803d560336b54e1b402becde0ec5f27
dogukanku <dogukan.kundum@argela.com.tr>
2020-07-24 15:22:21 +0300

    someone changed according to python3.

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ import pandas as pd
 # Control delimiters, rows, column names with read_csv (see later)
 data = pd.read_csv("/tweets_turkcell_end.csv", encoding='utf-8')
 # Preview the first 5 lines of the loaded data
-print data.head(53000)
+print (data.head(53000))
 dat = pd.DataFrame({'date': [], 'tweet': []})
 dat.to_csv('tweets_turkcell4.csv', encoding='utf-8')
